437d25329beb1a376a220ecc7bcbedac5d855f62
Plamen Georgiev <p.olegov@yahoo.com>
2023-01-20 13:43:20 +0200

    Some changes

--- src/js/Application.js
+++ src/js/Application.js
@@ -15,28 +15,30 @@ export default class Application extends EventEmitter {
         const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
         let count = 0;
 
-        const message = document.createElement("div");
-        message.classList.add("message");
-        message.innerText = "Ah";
+
+        // message.innerText = "Ah";
 
         this._beat = new Beat();
 
-        document.querySelector(".main").appendChild(message);
 
 
         this._beat.addListener(Beat.events.BIT, function () {
-            _create();
+            const message = document.createElement("div");
+            message.classList.add("message");
+            message.textContent = lyrics[count];
+            document.querySelector(".main").appendChild(message);
             count++;
             if(count === 6) {
                 count = 0;
             }
         });
 
-        const _create = () => {
-            message.textContent = lyrics[count];
-        }
 
         this.emit(Application.events.READY);
+
+    }
+
+    _create() {
 
     }
 
